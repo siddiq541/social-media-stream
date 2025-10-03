51354319512a6912ff1e9821a80a6bd52d093b96
Siddiq <m4siddiq@gmail.com>
2025-10-03 13:07:50 +0100

    posts.js file is added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import NavBar from '@/components/NavBar/NavBar';
 
 export const metadata = {
   title: 'Social Stream',
@@ -9,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 text-gray-900">
-        <Navigation />
+        <NavBar />
         {children}
         </body>
     </html>
